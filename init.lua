6672487ea2968c8b22d98960cdbbe78635853552
Jarrett <jarrett.git@endor.cyou>
2025-03-10 00:52:18 +0100

    Copy multiple cells at once

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ local function copy_text(start, stop)
   vim.fn.setreg("+", copied_text) -- Copy to system clipboard
   -- vim.fn.setreg('"', copied_text)  -- Copy to unnamed register
 end
-local function find_cell_start()
+local function get_cell_start()
   local cursor_pos = vim.api.nvim_win_get_cursor(0)[1] -- Get current line number
   for i = cursor_pos - 1, 1, -1 do
     local line = vim.api.nvim_buf_get_lines(0, i - 1, i, false)[1]
@@ -16,7 +16,7 @@ local function find_cell_start()
   end
   return 1
 end
-local function find_cell_end()
+local function get_cell_end()
   local cursor_pos = vim.api.nvim_win_get_cursor(0)[1] -- Get current line number
   local last_line = vim.api.nvim_buf_line_count(0)
   for i = cursor_pos + 1, last_line do
@@ -28,8 +28,8 @@ local function find_cell_end()
   return last_line
 end
 local function copy_cell()
-  local a = find_cell_start()
-  local b = find_cell_end()
+  local a = get_cell_start()
+  local b = get_cell_end()
   copy_text(a, b)
   vim.notify("Copied text: " .. a .. "," .. b .. ". " .. vim.log.levels.INFO)
 end
@@ -43,13 +43,19 @@ local function prev_cell()
   end
   return 1
 end
-local function next_cell()
-  local cursor_pos = vim.api.nvim_win_get_cursor(0)[1] -- Get current line number
-  local last_line = vim.api.nvim_buf_line_count(0) -- Get total number of lines in the buffer
+local function get_next_cell(opts)
+  opts = opts or { args = "1" } -- Default to "1" if opts is nil
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)[1]
+  local last_line = vim.api.nvim_buf_line_count(0)
+  local count = 0
+  local num_cells = tonumber(opts.args) or 1
   for i = cursor_pos + 1, last_line do -- Iterate downwards
     local line = vim.api.nvim_buf_get_lines(0, i - 1, i, false)[1]
     if line and line:find("%%") then
-      return i
+      count = count + 1
+      if count == num_cells then
+        return i
+      end
     end
   end
   return last_line
@@ -58,18 +64,20 @@ local function move_cursor(line)
   vim.api.nvim_win_set_cursor(0, { line, 0 }) -- Move cursor to the found line
 end
 local function move_next_cell()
-  local line = next_cell()
+  local line = get_next_cell()
   move_cursor(line)
 end
 local function move_prev_cell()
   local line = prev_cell()
   move_cursor(line)
 end
-local function copy_multiple_cells()
-  local start = find_cell_start()
-  local stop = next_cell() -- Move to the next cell
+local function copy_multiple_cells(opts)
+  local start = get_cell_start()
+  local stop = get_next_cell(opts) -- Move to the next cell
   copy_text(start, stop)
+  vim.notify("Copied cells from line " .. start .. " to line " .. stop .. ". ", vim.log.levels.INFO)
 end
+
 vim.api.nvim_set_keymap("n", "<leader>a", "<Nop>", { noremap = true, silent = true, desc = "Cell motions" })
 
 vim.api.nvim_create_user_command("Copycell", copy_cell, {})
@@ -80,11 +88,11 @@ vim.api.nvim_set_keymap(
   { noremap = true, silent = true, desc = "Yank current cell" }
 )
 
-vim.api.nvim_create_user_command("Movenextcell", move_next_cell, {})
+vim.api.nvim_create_user_command("Movenextcell", move_next_cell, { nargs = 1 })
 vim.api.nvim_set_keymap(
   "n",
   "<leader>an",
-  ":Movenextcell<CR>",
+  ":Movenextcell 1<CR>",
   { noremap = true, silent = true, desc = "Go next cell" }
 )
 
@@ -96,7 +104,7 @@ vim.api.nvim_set_keymap(
   { noremap = true, silent = true, desc = "Go prev cell" }
 )
 
-vim.api.nvim_create_user_command("Copymulticell", copy_multiple_cells, {})
+vim.api.nvim_create_user_command("Copymulticell", copy_multiple_cells, { nargs = 1 })
 vim.api.nvim_set_keymap(
   "n",
   "<leader>am",
